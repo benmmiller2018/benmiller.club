4c34faa61d88bbd684c9ab056bc09336911da44b
Benjamin Miller <benmill@cs.utexas.edu>
2020-10-01 15:09:03 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://media.giphy.com/media/PATw464L4E8A8/giphy.gif" alt="logo" />
+       <h2> who do you know here</h2>
+
       </header>
     </div>
   );
