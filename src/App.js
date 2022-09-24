bd5b50c8a885a8e5cf9cdb5d1a0a20adf8929203
benmmiller2018 <61123477+benmmiller2018@users.noreply.github.com>
2022-09-23 20:26:13 -0500

    .......

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 import './App.css';
 
-var date = new Date();
-var d = date.getMinutes();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://media.giphy.com/media/BEBkhdMsDbE5i/giphy.gif" alt="logo" />
-       <h2> who do you know here{d}</h2>
+       <h2> who do you know here</h2>
 
       </header>
     </div>
