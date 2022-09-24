6a985a8ef4fe4cff6b697d1cf093999b61a5f23e
Ben Miller <benmiller@Bens-MacBook-Air.local>
2022-09-23 21:38:02 -0500

    pics

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 
 import './App.css';
+import img1 from "./1.jpeg"
+import img2 from "./2.png"
+
+
 
 var date = new Date();
 var d = date.getMinutes();
+var img;
+if (d == 36){
+  img = img1;
+}
+else{
+  img = img2;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://media.giphy.com/media/BEBkhdMsDbE5i/giphy.gif" alt="logo" />
+        <img src={img} alt="logo" />
        <h2> who do you know here{d}</h2>
 
       </header>
